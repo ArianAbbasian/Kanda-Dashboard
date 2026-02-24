c311adbc00c8c36b80dd56cf5708193476ca2ccb
Arian Abbasian <138059626+ArianAbbasian@users.noreply.github.com>
2026-02-24 10:47:50 +0330

    Add Login and Register pages and Flow

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Login.Models;
+using Login.Data;
+
+
+
+
+public class AuthController : Controller
+{
+    private static List<User> users = new List<User>();
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        var user = users.FirstOrDefault(u =>
+            u.Username == username && u.Password == password);
+
+        if (user == null)
+        {
+            ViewBag.Error = "نام کاربری یا رمز اشتباه است";
+            return View();
+        }
+
+        HttpContext.Session.SetString("Username", user.Username);
+
+        return RedirectToAction("Profile", "User");
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(User user)
+    {
+        users.Add(user);
+        return RedirectToAction("Login");
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Login.Data;
+using Microsoft.AspNetCore.Mvc;
+using Login.Data;
+
+public class UserController : Controller
+{
+    public IActionResult Profile()
+    {
+        var username = HttpContext.Session.GetString("Username");
+
+        if (username == null)
+            return RedirectToAction("Login", "Auth");
+
+        var user = FakeDatabase.Users
+            .FirstOrDefault(u => u.Username == username);
+
+        return View(user);
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Login", "Auth");
+    }
+}
