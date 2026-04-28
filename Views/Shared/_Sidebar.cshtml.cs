2d3d21b36fcfdcb879f8eeda50710dbb02a2cda2
Arian <arianabusian84@gmail.com>
2026-04-28 22:44:59 +0330

    fix(ui): resolve UX bugs and polish visual appearance

---
+++ Views/Shared/_Sidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Login.Views.Shared
+{
+    public class _SidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
