50f5a7e37e4138ef7184c49ea150f5083f9e184f
Arian Abbasian <arianabusian013@gmail.com>
2026-05-08 23:05:55 +0330

    refactor(ui): replace legacy icons with FontAwesome, localize settings, and improve video page responsiveness

---
+++ Views/User/VideoPlayer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Login.Views.User
+{
+    public class VideoPlayerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
