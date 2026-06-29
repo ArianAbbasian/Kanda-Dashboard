6c33ca60b0987012187f8cd6d96a93dbf9b91c02
Arian Abbasian <arianabusian013@gmail.com>
2026-06-29 23:46:14 +0330

    chore: remove commented-out static files line

--- Program.cs
+++ Program.cs
@@ -19,7 +19,6 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-//app.UseStaticFiles();
 app.UseStaticFiles(new StaticFileOptions
 {
     ContentTypeProvider = new FileExtensionContentTypeProvider
